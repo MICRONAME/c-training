99345e6ca5d22016cf1d685f1070fa54a497ffdc
MICRONAME <m2311717@edu.misis.ru>
2025-07-28 12:43:06 +0300

    update

--- C-problems/problemPF.c
+++ C-problems/problemPF.c
@@ -1,3 +1,71 @@
-//
-// Created by Lenovo on 27.07.2025.
-//
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <malloc.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+__uint128_t gcd(__uint128_t a, __uint128_t b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+__uint128_t powmod(__uint128_t n, __uint128_t p, __uint128_t mod)
+{
+    __uint128_t mult = n % mod;
+    __uint128_t prod = 1;
+    while (p > 0)
+    {
+        if (p % 2 == 1)
+        {
+            prod = (prod * mult) % mod;
+        }
+        mult = (mult * mult) % mod;
+        p = p >> 1;
+    }
+    return prod;
+}
+
+int check(__uint128_t n)
+{
+    int count = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        __uint128_t x = rand();
+        if (powmod(x, n - 1, n) % n == 1 && gcd(x, n) == 1)
+        {
+            count++;
+        }
+    }
+    if (count >= 100) return 1;
+    return 0;
+}
+
+unsigned long long solve(__uint128_t a, __uint128_t b)
+{
+    __uint128_t f1 = 0, f2 = 1;
+    __uint128_t ans = 0;
+    while (f2 * a + f1 * b <= UINT64_MAX / (2 * 2 * 2 * 2))
+    {
+        __uint128_t temp = f2 * a + f1 * b;
+        if (check(temp) == 1)
+        {
+            ans = temp;
+            //printf("%llu%s", temp, "\n");
+        }
+        f1 = f2;
+        f2 = temp;
+    }
+    return ans;
+}
+
+int main()
+{
+    unsigned long long a, b, sc;
+    sc = scanf("%llu%llu", &a, &b);
+    assert(sc == 2);
+    assert(0ull < a && a < 256ull);
+    assert(0ull < b && b < 256ull);
+    if (gcd(a, b) != 1) printf("%d", 0);
+    else printf("%llu", solve(a, b));
+    //printf("%d", check(1000000007));
+}
--- C-problems/task-1.c
+++ C-problems/task-1.c
@@ -1,35 +1,18 @@
-//
-// Created by Lenovo on 27.07.2025.
-//
-//
-// Created by Lenovo on 16.07.2025.
-//
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
 #include <malloc.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-#if 0
-long long func(long long n, long long m)
-{
-    long long f1 = 0, f2 = 1;
-    if (n == 0) return 0;
-    for (long long i = 1; i < n; i++)
-    {
-        long long temp = f2;
-        f2 = (f2 + f1) % m;
-        f1 = temp;
-    }
-    return f1;
+__uint128_t gcd(__uint128_t a, __uint128_t b) {
+    return b == 0 ? a : gcd(b, a % b);
 }
-#endif
 
-#if 0
-// бинарное возведение в степень по модулю. запомнить!!!!!
-unsigned long long powmod(unsigned long long n, unsigned long long p, unsigned long long mod)
+__uint128_t powmod(__uint128_t n, __uint128_t p, __uint128_t mod)
 {
-    unsigned long long mult = n % mod;
-    unsigned long long prod = 1;
+    __uint128_t mult = n % mod;
+    __uint128_t prod = 1;
     while (p > 0)
     {
         if (p % 2 == 1)
@@ -41,64 +24,21 @@ unsigned long long powmod(unsigned long long n, unsigned long long p, unsigned l
     }
     return prod;
 }
-#endif
-
 
 int main()
 {
-#if 0
-    //задача нахождения первого и последнего бита числа
-    int n, i = 0;
-    int sc = scanf("%d", &n);
-    assert(sc == 1);
-    while (!(n & (1 << i)))
-    {
-        i++;
-    }
-    printf("%d%c", i, ' ');
-    i = 0;
-    while (!(n & (1 << (31 - i))))
-    {
-        i++;
-    }
-    printf("%d%c", 32 - i - 1, ' ');
-#endif
-#if 0
-    // invert k-th bit of sequence
-    int n, k, sc, m;
-    sc = scanf("%d", &n);
-    assert(sc == 1);
-    int *v = malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++)
-    {
-        sc = scanf("%d", &m);
-        assert(sc == 1);
-        v[i] = m;
-    }
-    sc = scanf("%d", &k);
-    assert(sc == 1);
-    v[k / 8] = v[k / 8] ^ (1 << (k % 8));
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d%c", v[i], ' ');
-    }
-#endif
-
-#if 0
-    // алгоритм проверки числа на простоту с помощью малой теоремы ферма и бинарного возведения в степень
     unsigned long long n, sc;
     int count = 0;
     sc = scanf("%llu", &n);
     assert(sc == 1);
-    for (unsigned long long i = 2; i < 202; i++)
+    for (int i = 0; i < 100; i++)
     {
-        if (powmod(i, n - 1, n) % n == 1ull && i % n != 0)
+        __uint128_t x = rand();
+        if (powmod(x, n - 1, n) % n == 1 && gcd(x, n) == 1)
         {
             count++;
         }
     }
-    if (count >= 100) printf("%c", '1');
+    if (count >= 10) printf("%c", '1');
     else printf("%c", '0');
-#endif
-
 }
