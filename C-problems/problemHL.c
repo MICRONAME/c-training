384ba158b783ec7b077019eb29f24a565fcc133e
MICRONAME <m2311717@edu.misis.ru>
2025-08-20 11:09:16 +0300

    cmake lists update

--- C-problems/problemHL.c
+++ C-problems/problemHL.c
@@ -1,3 +1,86 @@
-//
-// Created by Lenovo on 19.08.2025.
-//
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node_t
+{
+    int data;
+    struct node_t *next;
+};
+
+int loop_len(struct node_t *top) {
+    if (top == NULL)
+        return 0;
+
+    struct node_t *first = top;
+    struct node_t *second = top->next;
+    int p = 1;
+    int len = 1;
+
+    while (second != NULL && second != first) {
+        if (len == p) {
+            p *= 2;
+            len = 0;
+            first = second;
+        }
+        second = second->next;
+        ++len;
+    }
+    if (second == NULL)
+        return 0;
+    return len;
+}
+#if 0
+void print(struct node_t *top)
+{
+    for (struct node_t *p = top; p != NULL; p = p->next)
+        printf("%d", p->data);
+}
+
+void delete_list(struct node_t *top)
+{
+    while (top != NULL) top = top->next;
+}
+
+struct node_t * initialize(int n)
+{
+    int data;
+    struct node_t *top = NULL, *tail = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        struct node_t *new = calloc(1, sizeof(struct node_t));
+        scanf("%d", &data);
+        if (top == NULL)
+        {
+            top = new;
+            top->data = data;
+        }
+        else
+        {
+            if (tail == NULL)
+            {
+                tail = new;
+                top->next = tail;
+                tail->data = data;
+            }
+            else
+            {
+                tail->next = new;
+                tail = tail->next;
+                tail->data = data;
+            }
+        }
+    }
+    return top;
+}
+
+int main()
+{
+    int n, sc;
+    sc = scanf("%d", &n);
+    struct node_t *list = initialize(n);
+    int len = loop_len(list);
+    printf("%d\n", len);
+    print(list);
+    delete_list(list);
+}
+#endif
--- C-problems/problemLR.c
+++ C-problems/problemLR.c
@@ -1,3 +1,85 @@
-//
-// Created by Lenovo on 19.08.2025.
-//
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node_t
+{
+    int data;
+    struct node_t *next;
+};
+
+struct node_t *reverse(struct node_t *top) {
+    if (top == NULL) return NULL;
+    if (top->next == NULL) return top;
+    struct node_t *tail = top, *mid = top->next, *head = top->next->next;
+    if (head == NULL){
+        top->next->next = top;
+        top->next = NULL;
+        return mid;
+    }
+    while (head != NULL){
+        if (tail == top){
+            top->next = NULL;
+        }
+        mid->next = tail;
+        tail = mid; mid = head; head = head->next;
+    }
+    mid->next = tail;
+    return mid;
+}
+
+void delete_list(struct node_t *top)
+{
+    while (top != NULL) top = top->next;
+}
+
+void print(struct node_t *top)
+{
+    for (struct node_t *p = top; p != NULL; p = p->next)
+        printf("%d", p->data);
+    printf("\n");
+}
+
+struct node_t * initialize(int n)
+{
+    int data;
+    struct node_t *top = NULL, *tail = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        struct node_t *new = calloc(1, sizeof(struct node_t));
+        scanf("%d", &data);
+        if (top == NULL)
+        {
+            top = new;
+            top->data = data;
+        }
+        else
+        {
+            if (tail == NULL)
+            {
+                tail = new;
+                top->next = tail;
+                tail->data = data;
+            }
+            else
+            {
+                tail->next = new;
+                tail = tail->next;
+                tail->data = data;
+            }
+        }
+    }
+    return top;
+}
+
+int main()
+{
+    int n, sc;
+    sc = scanf("%d", &n);
+    assert(sc == 1);
+    struct node_t *list = initialize(n);
+    print(list);
+    struct node_t *reversed = reverse(list);
+    print(reversed);
+    delete_list(list);
+}
